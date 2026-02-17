1c1f153746b412b7278a60ab7dd8e412ca4003ff
TaNoir <evanderamorcillo@gmail.com>
2026-02-18 05:53:31 +0800

    feat: add unique index to Product SKU column

--- MobileOpsConnect/Data/Migrations/20260217215046_AddSkuUniqueIndex.Designer.cs
+++ MobileOpsConnect/Data/Migrations/20260217215046_AddSkuUniqueIndex.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MobileOpsConnect.Data;
 
@@ -11,9 +12,11 @@ using MobileOpsConnect.Data;
 namespace MobileOpsConnect.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260217215046_AddSkuUniqueIndex")]
+    partial class AddSkuUniqueIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
